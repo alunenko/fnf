2a93625254d6b1b43cbf0058c2fa2b7f62c286f9
Antoni Lunenko <antoni.lunenko@gmail.com>
2015-02-18 02:58:34 +0200

    create post from file, later it will be from db

--- index.php
+++ index.php
@@ -4,10 +4,46 @@
     <title>FNF</title>
 </head>
     <body>
+
+    <!-- primitive, prototype, crutch logic... chaotic things -> from my head to code -->
+    <?php
+        $handle = @fopen("output.csv", "r");
+        if ($handle) {
+            while (($buffer = fgets($handle, 4096)) !== false) {
+                #echo $buffer;
+                $res[] = explode(",", $buffer);
+            }
+            if (!feof($handle)) {
+                echo "Error: unexpected fgets() fail\n";
+            }
+            fclose($handle);
+        }
+
+        foreach ($res as $str => $value) {
+            foreach ($value as $item => $itemVal) {
+                if (strpos($itemVal, 'youtu') !== false) {
+                    #print_r($value);die('123');
+                    $pubdate = date("m/d/Y h:m:s", substr($value[1], 1, -1));
+
+                    $startPos = strpos($itemVal, 'http', 20);
+                    $endPos = strpos($itemVal, '</a>');
+                    
+                    $youtubeLinks[] = str_replace("watch?v=", "v/", substr($itemVal, $startPos, $endPos - $startPos));
+                } else {
+                    $otherLinks[] = $itemVal;
+                }
+            }
+        }
+    ?>
+
+<!-- wrapper -->
         <div style="
     width: 960px;
     margin: 0 auto;
 ">
+
+
+<!-- post -->
             <div style="
     margin-bottom: 50px;
     border-bottom: 1px solid;
@@ -22,15 +58,21 @@
                 <p>posted <span><?php echo date('m/d/Y h:i:s') ?></span></p>
                 <img src="<?php echo 'images/'.$img_name ?>" alt="img">
             </div>
+<!-- end post -->
 
-            <div style="
+<!-- post -->
+    <?php
+            echo '<div style="
     margin-bottom: 50px;
     border-bottom: 1px solid;
     padding-bottom: 25px;
-">
-<p>posted <span><?php echo date('m/d/Y h:i:s') ?></span></p>
-<iframe width="420" height="315" src="https://www.youtube.com/embed/sVby7a2dpr8" frameborder="0" allowfullscreen></iframe>
-            </div>
+">';
+        foreach ($youtubeLinks as $link => $linkVal) {
+            echo '<p>posted <span>' . $pubdate . '</span></p><iframe width="420" height="315" src="' . $linkVal . '" frameborder="0" allowfullscreen></iframe>';
+        }
+            echo '</div>';
+    ?>
+<!-- end post -->
 
             
         </div>
