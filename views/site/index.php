1176dc651e557e5fbdcceb93d1ad578b1d613da8
Lunenko Anton <lunenko@qarea.com>
2015-02-20 20:55:06 +0200

    add if statements for diff links

--- views/site/index.php
+++ views/site/index.php
@@ -24,13 +24,55 @@ $this->title = 'Fun/news feed';
                 <p><?php echo /*$link->author,*/ '<span>' . Yii::$app->formatter->asDate($link->timestamp, 'M/d/Y H:i:s') . '</span>' ?></p>
                 <p>
                     <?php
-                        if (strpos($link->link,'youtu') !== false ) {
+                        if (strpos($link->link, 'youtu') !== false ) {
                             $startPos = strpos($link->link, '">');
                             $endPos = strpos($link->link, '</a>');
 
                             echo '<iframe width="420" height="315" src="' . str_replace("watch?v=", "v/", substr($link->link, $startPos+2, $endPos - $startPos-2)) . '" frameborder="0" allowfullscreen></iframe>';
-                        } else {
-                            echo $link->link;
+                        } else if ((strpos($link->link, 'jpg') !== false) || (strpos($link->link, 'gif') !== false) || (strpos($link->link, 'jpeg') !== false) || (strpos($link->link, 'png') !== false) || (strpos($link->link, 'vk.com/doc') !== false)) {
+                            $startPos = strpos($link->link, '">');
+                            $endPos = strpos($link->link, '</a>');
+
+                            $file = substr($link->link, $startPos+2, $endPos - $startPos-2);
+                            $file_headers = @get_headers($file);
+                            if(($file_headers[0] == 'HTTP/1.1 404') || ($file_headers[0] == 'HTTP/1.1 302 Moved Temporarily')) {
+                                echo '404';
+                            }
+                            else {
+                                $startPos = strpos($link->link, '">');
+                                $endPos = strpos($link->link, '</a>');
+                                echo '<img src="'. substr($link->link, $startPos+2, $endPos - $startPos-2) . '"/>';   
+                            }
+                        } else if (strpos($link->link, 'coub') !== false) {
+                            $startPos = strpos($link->link, '">');
+                            $endPos = strpos($link->link, '</a>');
+                            $coubLink = substr($link->link, $startPos+2, $endPos - $startPos-2);
+
+                            $startPos = strpos($coubLink, 'view/');
+                            $endPos = strlen($coubLink);
+                            $coubLink = substr($coubLink, $startPos+5, $endPos);
+
+                            echo '<iframe src="//coub.com/embed/' . $coubLink . '?muted=false&autostart=false&originalSize=false&hideTopBar=false&startWithHD=false" allowfullscreen="true" frameborder="0" width="640" height="360"></iframe>';
+                        } /*else if (strpos($link->link, 'facebook') !== false) {
+                            $startPos = strpos($link->link, 'v=');
+                            $endPos = strpos($link->link, '&');
+                            $facebook = substr($link->link, $startPos+2, $endPos - $startPos-2);
+
+                            echo $facebook;
+
+                            echo '<object width="400" height="224" > 
+                             <param name="allowfullscreen" value="true" /> 
+                             <param name="allowscriptaccess" value="always" /> 
+                             <param name="movie" value="http://www.facebook.com/v/xxx" /> 
+                             <embed src="http://www.facebook.com/video.php?v=' . $facebook . '" type="application/x-shockwave-flash"  
+                               allowscriptaccess="always" allowfullscreen="true" width="400" height="224"> 
+                             </embed> 
+                            </object>';
+                        }*/ else {
+                            $startPos = strpos($link->link, 'href="');
+                            $endPos = strpos($link->link, '">', $startPos+strlen('href="'));
+                            $usualLink = substr($link->link, $startPos+strlen('href="'), $endPos - ($startPos+strlen('href="')));
+                            echo '<a style="font-size: 28px" href="' . $usualLink . '">'. $usualLink . '</a>';
                         }
                     ?>
 
